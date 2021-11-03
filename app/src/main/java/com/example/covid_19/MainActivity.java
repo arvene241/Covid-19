3d245857b3334651a0dd42e92c6c9d42929850c7
Mike <arvene241@gmail.com>
2021-11-03 18:02:22 +0800

    add listener to the buttons

--- app/src/main/java/com/example/covid_19/MainActivity.java
+++ app/src/main/java/com/example/covid_19/MainActivity.java
@@ -2,16 +2,54 @@ package com.example.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonCall, buttonSms, buttonTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCall = findViewById(R.id.callBtn);
+        buttonSms = findViewById(R.id.smsBtn);
+//        buttonTest = findViewById(R.id.testBtn);
+
+        buttonCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL); // to open the dial in the phone
+                intent.setData(Uri.parse("tel:911")); // were setting the 911 to the phone call
+                startActivity(intent); // to start the activity
+            }
+        });
+
+        buttonSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setType("vnd.android-dir/mms-sms");
+                startActivity(intent);
+            }
+        });
+
+//        buttonTest.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String url = "https://www.cdc.gov/coronavirus/2019-ncov/testing/self-testing.html";
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse(url));
+//                startActivity(intent);
+//            }
+//        });
     }
 }
