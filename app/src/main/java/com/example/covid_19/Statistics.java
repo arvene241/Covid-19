f3d06bde34c747044bb7ea28ee9484088f342369
Mike <arvene241@gmail.com>
2021-12-02 16:29:23 +0800

    finished app

--- app/src/main/java/com/example/covid_19/Statistics.java
+++ app/src/main/java/com/example/covid_19/Statistics.java
@@ -3,6 +3,7 @@ package com.example.covid_19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,7 +27,7 @@ public class Statistics extends AppCompatActivity  {
     String country;
 
     ImageView back;
-    Button buttonTotal, buttonToday;
+    Button graphButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,7 @@ public class Statistics extends AppCompatActivity  {
         setContentView(R.layout.activity_statistics);
 
         back = findViewById(R.id.imageView);
+        graphButton = findViewById(R.id.graphButton);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +44,16 @@ public class Statistics extends AppCompatActivity  {
                 Intent i = new Intent(Statistics.this, MainActivity.class);
                 startActivity(i);
                 finish();
+            }
+        });
+
+        graphButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://covid19.who.int";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
             }
         });
 
